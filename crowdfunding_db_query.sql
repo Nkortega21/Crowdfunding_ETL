95d950188b99f1dc940220696c64d576321da50b
craha001 <79893932+craha001@users.noreply.github.com>
2024-09-19 19:44:50 -0700

    Add files via upload

---
+++ crowdfunding_db_query.sql
@@ -0,0 +1,8 @@
+-- Query to visualize each table and ensure the proper data is found within them
+select * from campaign limit 10;
+
+select * from contacts limit 10;
+
+select * from category limit 10;
+
+select * from subcategory limit 10;
